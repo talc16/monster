e64698a40251093bf534e2ead5d818f414e2098f
Tal Cohen <Talc16@gmail.com>
2019-07-23 21:39:48 +0300

    final

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 
+
+let selected = ""
+if (selected) colorSelected(selected)
 const mainBoard = document.querySelector("#main");
 const monster1 = new Monster("shrek", "green",
-    "https://i.ytimg.com/vi/LEmTf7A5Xl4/maxresdefault.jpg", { x: "400px", y: "400px" })
+    "https://static.boredpanda.com/blog/wp-content/uploads/2017/10/creepy-pokemon-david-szilagyi-102-59d33db716ee4-png__880.jpg", { x: randomNumber(1000) + "px", y: randomNumber(1000) + "px" })
 const monster2 = new Monster("clown", "funny",
-    "https://cdn.ebaumsworld.com/mediaFiles/picture/604025/85925971.jpg", { x: "300px", y: "200px" })
+    "https://static.boredpanda.com/blog/wp-content/uploads/2017/10/creepy-pokemon-david-szilagyi-35-59d33e192ff0b__880.jpg", { x: randomNumber(1000) + "px", y: randomNumber(1000) + "px" })
 const monster3 = new Monster("woman", "in red",
-    "https://images.pexels.com/photos/247122/pexels-photo-247122.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", { x: "500px", y: "100px" })
+    "https://static.boredpanda.com/blog/wp-content/uploads/2017/10/creepy-pokemon-david-szilagyi-45-59d33d053ae6e__880.jpg", { x: randomNumber(1000) + "px", y: randomNumber(1000) + "px" })
+
+const monster4 = new Monster("poki", "in red",
+    "https://static.boredpanda.com/blog/wp-content/uploads/2017/10/creepy-pokemon-david-szilagyi-98-59d33dabcebb8__880.jpg", { x: randomNumber(1000) + "px", y: randomNumber(1000) + "px" })
+
 
 const monsters = [];
-//
-monsters.push(monster1, monster2, monster3)
 
-for (let index = 0; index < monsters.length; index++) {
 
+document.getElementById("clickRandom").addEventListener("click", function () {
+    randomPlace()
+});
+
+function randomNumber(multi) {
+    let num = `${Math.round(Math.random() * multi)}`;
+    return num
 }
+
+monsters.push(monster1, monster2, monster3, monster4)
+
 
 monsters.forEach(function (monster) {
     console.log(monster)
@@ -22,7 +36,7 @@ monsters.forEach(function (monster) {
 function draw(arr) {
     mainBoard.innerHTML = "";
     arr.forEach(function (monster) {
-        mainBoard.append(monster.getCard(200, 200))
+        mainBoard.append(monster.getCard(300, 300))
 
     });
 }
@@ -30,15 +44,51 @@ function draw(arr) {
 
 draw(monsters)
 
-setTimeout(() => {
-    monster1.setPicture("https://cdn.ebaumsworld.com/thumbs/2019/04/02/072350/85925938/creepy2.jpg")
-    monster1.setXpos(20)
-    draw(monsters)
-}, 1000)
 
-
-setTimeout(() => {
-    monster1.setXpos(400)
+function randomPlace() {
+    monsters.forEach(function (monster) {
+        monster.randomLocation(randomNumber(1000), randomNumber(1000))
+        draw(monsters)
+    });
+};
+function moveThis(monster, x, y) {
+    monster.newLocation(x, y)
     draw(monsters)
-}, 4000)
+    colorSelected(selected)
+};
+
+function colorSelected(monsterSelect) {
+    selected = monsterSelect
+    let allcolorBorder = document.querySelectorAll(".monster")
+    let colorBorder = document.getElementsByClassName(selected.id)
+    for (let index = 0; index < allcolorBorder.length; index++) {
+        allcolorBorder[index].style.border = ""
+
+    }
+
+    colorBorder[0].style.border = "3px solid yellow"
+
+}
+
+function keyDown(monsterSelect) {
+    colorSelected(monsterSelect)
+
+    document.onkeydown = function (e) {
+        switch (e.keyCode) {
+            case 37:
+                monsterSelect.buttonClickLeft()
+                break;
+            case 38:
+                monsterSelect.buttonClickUp()
+                break;
+            case 39:
+                monsterSelect.buttonClickRight()
+                break;
+            case 40:
+                monsterSelect.buttonClickDown()
+                break;
+        }
+    };
+}
+
 
--- models.js
+++ models.js
@@ -6,11 +6,10 @@ class Monster {
         this.id = `monster_${Math.round(Math.random() * 999)}`;
         this.name = name;
         this.lastName = lastName;
-        this.imageUrl = imageUrl
-        this.x = pos.x
-        this.y = pos.y
-
-
+        this.imageUrl = imageUrl;
+        this.x = pos.x;
+        this.y = pos.y;
+        
     }
 
     //behaviour
@@ -23,22 +22,78 @@ class Monster {
         this.imageUrl = pic
     }
 
-    setXpos(xpos) {
-        // this.x = parseInt(this.x) + xpos + "px"
-        this.x = `${parseInt(this.x) + xpos}px`
-    }
+    // setXpos(xpos) {
+    //     // this.x = parseInt(this.x) + xpos + "px"
+    //     this.x = `${parseInt(this.x) + xpos}px`
+    // }
 
     getCard(w, h) {
         const monsterUI = document.createElement("div")
+        monsterUI.classList.add(this.id, "monster")
+        
         const img = document.createElement("img");
+        img.addEventListener("click",this.selectedOne.bind(this))
+
+
         img.src = this.imageUrl
         img.height = w;
         img.width = h;
-        monsterUI.append(img)
+        const buttonDiv = document.createElement("div")
+        buttonDiv.style.background="red"
+        const buttonUp = document.createElement("button")
+        buttonUp.className="btn btn-primary btnUp"
+        buttonUp.innerText="UP!"
+        buttonUp.addEventListener("click",this.buttonClickUp.bind(this))
+
+        const btnDown = document.createElement("button")
+        btnDown.className="btn btn-primary btnDown"
+        btnDown.innerText="Down!"
+        btnDown.addEventListener("click",this.buttonClickDown.bind(this))
+        
+        const btnleft = document.createElement("button")
+        btnleft.className="btn btn-primary btnleft"
+        btnleft.innerText="Left!"
+        btnleft.addEventListener("click",this.buttonClickLeft.bind(this))
+      
+        const btnRight = document.createElement("button")
+        btnRight.className="btn btn-primary btnleft"
+        btnRight.innerText="Right!"
+        btnRight.addEventListener("click",this.buttonClickRight.bind(this))
+
+        buttonDiv.append(buttonUp,btnDown,btnleft,btnRight)
+        monsterUI.append(buttonDiv,img)
         monsterUI.style.position = "absolute"
         monsterUI.style.left = this.x
         monsterUI.style.top = this.y
         return monsterUI;
+    }
+
+    selectedOne(){
+        keyDown(this)
+        
+    }
+
+
+    buttonClickUp(){
+        moveThis(this,randomNumber(0),-randomNumber(10))
+    }
+    buttonClickDown(){
+        moveThis(this,randomNumber(0),randomNumber(10))
+    }
+    buttonClickLeft(){
+        moveThis(this,-randomNumber(10),randomNumber(0))
+    }
+    buttonClickRight(){
+        moveThis(this,randomNumber(10),randomNumber(0))
+    }
+
+  randomLocation(x,y) {
+        this.x = `${x}px`
+        this.y = `${y}px`
+    }
+  newLocation(x,y) {
+        this.x = `${parseInt(this.x)+parseInt(x)}px`
+        this.y = `${parseInt(this.y) + parseInt(y)}px`
     }
 
 }
